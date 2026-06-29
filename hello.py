3627bd3577a801aa648a0a1442906e90dbb01ace
Tyler <kaiser123293@gmail.com>
2026-06-29 00:42:57 -0400

    power calculator exercise

--- hello.py
+++ hello.py
@@ -1,2 +1,24 @@
 name = "Tyler"
 print("Hello," + name)
+print(2+3)
+print(10-4)
+print(3*7)
+print(10//3)
+voltage=220
+current=12
+power=voltage*current
+print(power)
+# One is for divison and the other is interger division. % is modulus/remainder
+first="Tyler"
+last="Kaiser"
+full_name=first + " " + last
+print(full_name)
+print(len(full_name))
+name=input("what is your name?")
+print("hello, " +name)
+# ask the user for a voltage
+volatge = float(input("Enter voltage:"))
+# ask the user for a current
+current = float(input("Enter Current: "))
+power = voltage * current
+print(power)
